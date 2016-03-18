57eeec19c360d8fb14decf6281c29deb02c45fbf
xinliu <xinliu1230@gmail.com>
2016-03-18 03:04:47 -0700

    rewrite operations

--- src/main/java/Operations/DeleteOp.java
+++ src/main/java/Operations/DeleteOp.java
@@ -37,35 +37,43 @@ import com.yahoo.ycsb.DB;
  * modified by Xin on 16/2/24
  */
 
-public class DeleteOp extends Operation {
+public class DeleteOp extends WriteOp {
+    public Durability durability = Durability.USE_DEFAULT;
 
-    private HashMap<String,ByteIterator> preValues;
+ //   private HashMap<String,ByteIterator> preValues;
 
-    public DeleteOp (String table, String key, Set<String> fields) {
+    public DeleteOp (String table, String key) {
 	this.table = table;
 	this.key = key;
-	preValues = new HashMap<String,ByteIterator>();
     }
 
     @Override
-    public Status doOp(DB db) {
-	//get previous values for the row of (table: key)
-	Status readPre = db.read(table, key, null, preValues);
-	//error when read previous values
-	if( readPre.equals(Status.ERROR) ) return Status.ERROR;
-	//row not found
-	else if( readPre.equals(Status.NOT_FOUND) ) {
-		preValues = null;
-		return Status.OK;
-	}
-	return db.delete(table, key);
+    public Status doOp(Connection connection) {
+      TableName tName = TableName.valueOf(table);
+      byte[] columnFamilyBytes = Bytes.toBytes(columnFamily);
+      Table currentTable;
+      try {
+        currentTable = connection.getTable(tName);
+      } catch (IOException e) {
+        System.err.println("Error accessing HBase table: " + e);
+        return Status.ERROR;
+      }
+
+    Delete d = new Delete(Bytes.toBytes(key));
+    d.setDurability(durability);
+    try {
+        currentTable.delete(d);
+    } catch (IOException e) {
+      return Status.ERROR;
     }
 
+    // update the timestamp for table:key in the table "TimeStampTable"
+    updateTimetamp(table, key, connection);
+    return Status.OK;	
+  }
+
     @Override
-    public Status undoOp(DB db) {
-	if(preValues == null) {
-        	return Status.OK;
-	}
-	return db.insert(table, key, preValues);
+    public Status undoOp(Connection connection) {
+	return Status.OK;
     }
 }
